f4d1bfbdf1abe4e0fe48fe397e0c9bd556da5d91
Martin Bachmann <martinpezbach@gmail.com>
2023-02-07 17:57:38 -0300

    feat: add json reader to import data function

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -7,7 +8,10 @@ class Inventory:
     @staticmethod
     def import_data(file_path, type):
         with open(file_path) as file:
-            data = list(csv.DictReader(file))
+            if file_path.endswith(".csv"):
+                data = list(csv.DictReader(file))
+            elif file_path.endswith(".json"):
+                data = json.load(file)
 
             if type == "completo":
                 return CompleteReport.generate(data)
